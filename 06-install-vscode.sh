5a497658478ce9c67bdf8d66c5d9e47605ac5c3b
Markus Kofler <59422120+mkoflerAT@users.noreply.github.com>
2026-01-29 04:42:26 +0000

    Fix auto-installer for vscode

--- 06-install-vscode.sh
+++ 06-install-vscode.sh
@@ -6,6 +6,7 @@
 echo 'Install vscode'
 wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/6f17636121051a53c88d3e605c491d22af2ba755/code_1.103.2-1755709794_amd64.deb
 mv ./code_1.103.2-1755709794_amd64.deb /tmp/
+echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
 sudo apt install -y /tmp/code_1.103.2-1755709794_amd64.deb
 rm /tmp/code_1.103.2-1755709794_amd64.deb
 echo '-----------------------------------------------------------------------'
