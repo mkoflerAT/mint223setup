2ed825455fba237ec0555fa611583d4ba67c4234
Markus Kofler <59422120+mkoflerAT@users.noreply.github.com>
2026-01-29 16:10:25 +0000

    Fix PowerShell-functions path

--- post-install-powershell-full.ps1
+++ post-install-powershell-full.ps1
@@ -1,4 +1,5 @@
 ## post-install-powershell-full.ps1
+
 # Ensure profile directory and file exist, install posh-git and ensure it's imported
 $profileDir = Split-Path -Parent $PROFILE
 if (!(Test-Path $profileDir)) { New-Item -ItemType Directory -Path $profileDir -Force | Out-Null }
@@ -11,13 +12,16 @@ if (-not (Get-Content -Path $PROFILE -ErrorAction SilentlyContinue | Select-Stri
     Add-Content -Path $PROFILE -Value 'Import-Module posh-git' -Encoding UTF8
 }
 
+# Ensure the custom functions directory exists
+mkdir -p $HOME/PowerShell/functions/
+
 # Add a reusable code-block to load custom functions into the profile
 $codeBlock = @'
 # -----------------------------------------------------------------------------------------------------------------
 # Load custom functions [BEGIN]
 # -----------------------------------------------------------------------------------------------------------------
 Write-Host ('-' * 100)
-$functionsPath = "$HOME\Documents\WindowsPowershell\functions\*.ps1"
+$functionsPath = "$HOME/PowerShell/functions/*.ps1"
 if (Test-Path $functionsPath) { gci $functionsPath | % { Write-Host "Load functions: $($_.Name)"; . $_.FullName } }
 else { "No functions in $functionsPath - skip loading." | Write-Host }
 Write-Host ('-' * 100); Write-Host
