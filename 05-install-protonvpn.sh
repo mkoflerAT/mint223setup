34bf8aa2e447d5588200ec35fa1a29f63a4f14bd
Markus Kofler <59422120+mkoflerAT@users.noreply.github.com>
2026-01-29 02:50:24 +0000

    Fix ProtonVPN-installer by 'DEBIAN_FRONTEND=noninteractive'

--- 05-install-protonvpn.sh
+++ 05-install-protonvpn.sh
@@ -5,12 +5,11 @@
 
 echo 'Installing ProtonVPN...'
 wget https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.8_all.deb
-sudo dpkg -i ./protonvpn-stable-release_1.0.8_all.deb && sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive dpkg -i ./protonvpn-stable-release_1.0.8_all.deb && sudo apt update
 # sudo apt-get install protonvpn protonvpn-cli -y
-sudo apt install -y proton-vpn-gnome-desktop
+sudo DEBIAN_FRONTEND=noninteractive apt install -y proton-vpn-gnome-desktop
 echo 'Clean-up downloaded packages...'
 rm ./protonvpn-stable-release_1.0.8_all.deb
 echo 'Installing tray-icon for ProtonVPN...'
-sudo apt install -y libayatana-appindicator3-1 gir1.2-ayatanaappindicator3-0.1 gnome-shell-extension-appindicator
+sudo DEBIAN_FRONTEND=noninteractive apt install -y libayatana-appindicator3-1 gir1.2-ayatanaappindicator3-0.1 gnome-shell-extension-appindicator
 echo 'ProtonVPN should now be installed in your system. After starting ProtonVPN you should see the tray-icon.'
-
