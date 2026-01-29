ea25c00d2bb13ba68222005c8b15f7430c973fb9
Markus Kofler <59422120+mkoflerAT@users.noreply.github.com>
2026-01-29 12:31:54 +0000

    Add PowerShell profile setup with posh-git installation

--- 08-install-dotnet-core.sh
+++ 08-install-dotnet-core.sh
@@ -3,7 +3,6 @@
 # https://learn.microsoft.com/en-us/aspnet/core/security/enforcing-ssl?view=aspnetcore-9.0&tabs=visual-studio%2Clinux-sles#trust-the-aspnet-core-https-development-certificate
 # https://learn.microsoft.com/en-us/aspnet/core/security/enforcing-ssl?view=aspnetcore-8.0&tabs=visual-studio%2Clinux-sles#trust-https-certificate-on-linux
 
-sudo apt install -y dotnet-sdk-8.0
 sudo apt install -y dotnet-sdk-10.0
 sudo apt install -y libnss3-tools
 # PATH=$PATH:~/.dotnet/tools
--- 09-install-powershell.sh
+++ 09-install-powershell.sh
@@ -11,3 +11,13 @@ echo '-----------------------------------------------------------------------'
 echo 'powershell should have been installed - run it by "pwsh"'
 echo '-----------------------------------------------------------------------'
 
+echo "Setting up PowerShell profile with posh-git..."
+pwsh -NoProfile -Command "
+    \$profileDir = Split-Path -Parent \$PROFILE
+    if (!(Test-Path \$profileDir)) { New-Item -ItemType Directory -Path \$profileDir -Force | Out-Null }
+    if (!(Test-Path \$PROFILE)) { New-Item -ItemType File -Path \$PROFILE -Force | Out-Null }
+    Install-Module posh-git -Repository PSGallery -Force -AllowClobber
+    Add-Content -Path \$PROFILE -Value 'Import-Module posh-git' -Encoding UTF8
+"
+echo "PowerShell profile setup complete"
+echo '-----------------------------------------------------------------------'
